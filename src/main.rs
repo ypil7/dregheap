2bebe898d748db8f7ec4f2e6e79cf0fc7182e571
Yan P <y.pilon9@gmail.com>
2026-07-09 22:46:16 -0400

    feat: parse command arguments and bind tcp listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, Dreg Heap!");
+use tokio::net::{TcpListener};
+use clap::Parser;
+
+mod cli;
+
+#[tokio::main]
+async fn main() {
+    let args = cli::Args::parse();
+    let ip_addr = format!("0.0.0.0:{}", args.port);
+
+    let listener = TcpListener::bind(&ip_addr).await
+        .unwrap_or_else( |e| panic!("Failed to bind TCP Listener to: {}, error: {}", &ip_addr, e));
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        if let Err(e) = socket.try_write(&[1,2,4]) {
+            panic!("Failed writing bites to buffer: {}", e)
+        }
+    }
 }
